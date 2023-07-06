449a6fc30f40c2392d4be67c841ab4e090b09593
Matt Hall <matt-598@virginmedia.com>
2023-07-06 19:19:59 +0100

    Further functionality added.

--- bookreviews/bookreviews.py
+++ bookreviews/bookreviews.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 import os
 import math
+import time
 
 # script_dir = os.path.dirname(__file__)
 # rel_path_to_driver = "../drivers/chromedriver"
@@ -73,11 +74,53 @@ class BookReviews(webdriver.Chrome):
             review_urls.append(review_url)
         return review_urls
 
+    def check_for_popup(self):
+        time.sleep(2)
+        try:
+            popup = self.find_element(By.XPATH, "//div[@class='Overlay Overlay--floating']")
+            close_button = popup.find_element(By.XPATH, "//i[@class='Icon CloseIcon']")
+            close_button.click()
+            time.sleep(2)
+        except:
+            pass
+
     def select_english_filter(self):
+        button = self.find_element(By.XPATH,"//button[@class='Button Button--secondary Button--medium']")
+        button.click()
+        time.sleep(2)
+        languages = self.find_elements(By.XPATH,"//div[@class='RadioGroup']")[-1]
+        english = languages.find_element(By.XPATH, ".//label[@for='en']")
+        english.click()
+        time.sleep(2)
+        apply_button = self.find_element(By.XPATH, "//span[@class='Button__labelItem' and text()='Apply']")
+        apply_button.click()
+        time.sleep(2)
         return
 
+    # This class method requires logging into goodreads, which I don't want to
+    # hardcode in and publish on my github. However, if already logged in then
+    # it can be used as is.
+    # def load_more_reviews(self):
+    #     bottom_divider = self.find_element(By.XPATH, "//div[@class='Divider Divider--contents Divider--largeMargin']")
+    #     load_more_button = bottom_divider.find_element(By.XPATH, "//span[@class='Button__labelItem']")
+    #     load_more_button.click()
+    #     time.sleep(3)
 
-    def get_reviews_for_books(self, review_urls):
+    def get_reviews(self):
+        reviews = self.find_elements(By.XPATH, "//article[@class='ReviewCard']")
+        single_book_reviews = []
+        for review in reviews:
+            review_string = self.find_element(By.XPATH, "//span[@class='RatingStars RatingStars__small']").get_attribute("aria-label")
+            review_score = review_string[7]
+            all_text = self.find_element(By.XPATH, "//div[@class='TruncatedContent']")
+            review_text = all_text.find_element(By.XPATH, "//span[@class='Formatted']").text
+            single_book_reviews.append([review_score, review_text])
+        return single_book_reviews
+
+    def get_reviews_for_all_books(self, review_urls):
+        reviews_dataset = []
+        reviews_dataset.append(['review score', 'review text'])
         for review_url in review_urls:
-            self.get(review_url)
-        return
+            single_book_reviews = self.get_reviews(review_url)
+            reviews_dataset.append(single_book_reviews)
+        return reviews_dataset
