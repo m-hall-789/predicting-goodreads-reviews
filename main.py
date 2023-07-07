487aa804b28c634670453122e94657b3482b21a5
Matt Hall <matt-598@virginmedia.com>
2023-07-07 18:33:24 +0100

    Added relative path of driver to constants, so more easily modified.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from bookreviews.bookreviews import BookReviews
+import bookreviews.constants as const
 import os
 
 script_dir = os.path.dirname(__file__)
-rel_path_to_driver = "../drivers/chromedriver"
+rel_path_to_driver = const.DRIVER_REL_PATH
 abs_file_path_to_driver = os.path.join(script_dir, rel_path_to_driver)
 
 with BookReviews(abs_file_path_to_driver) as br:
